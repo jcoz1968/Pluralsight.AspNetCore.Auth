6379a249f199bbb4e19d04f284ca84c827e7fd85
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-03 14:11:31 -0500

    Facebook authentication

---
+++ Pluralsight.AspNetCore.Auth.Web/Controllers/FacebookAuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AspNetCore.Auth.Web.Controllers
+{
+	[Route("facebookauth")]
+	public class FacebookAuthController : Controller
+	{
+		[Route("signin")]
+		public IActionResult SignIn()
+		{
+			return Challenge(new AuthenticationProperties { RedirectUri = "/" });
+		}
+
+		[Route("signout")]
+		[HttpPost]
+		public async Task<IActionResult> SignOut()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
