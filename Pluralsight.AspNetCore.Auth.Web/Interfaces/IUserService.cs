2900d89063b6d60c1df2f0bea515faadc039fe2b
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-03 12:05:46 -0500

    Project Setup

---
+++ Pluralsight.AspNetCore.Auth.Web/Interfaces/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AspNetCore.Auth.Web.Interfaces
+{
+    public interface IUserService
+    {
+        Task<bool> ValidateCredentials(string username, string password, out User user);
+        Task<bool> AddUser(string username, string password);
+    }
+
+    public class User
+    {
+        public User(string username)
+        {
+            Username = username;
+        }
+
+        public string Username { get; }
+    }
+}
---
+++ Pluralsight.AspNetCore.Auth.Web/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pluralsight.AspNetCore.Auth.Web.Interfaces;
+using Pluralsight.AspNetCore.Auth.Web.Services;
+using System.Collections.Generic;
+
+namespace Pluralsight.AspNetCore.Auth.Web
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc(options => {
+				options.Filters.Add(new RequireHttpsAttribute());
+			});
+
+			var users = new Dictionary<string, string> { { "Chris", "password" } };
+			services.AddSingleton<IUserService>(new DummyUserService(users));
+
+			services.AddAuthentication(options => {
+				options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			})
+			.AddCookie(options => { options.LoginPath = "/auth/signin"; });
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseRewriter(new RewriteOptions().AddRedirectToHttps(301, 44343));
+			app.UseStaticFiles();
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
